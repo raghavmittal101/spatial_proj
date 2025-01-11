1354e4656de14d3411c84b70abcb510de936a313
Raghav Mittal <raghavmittal101@users.noreply.github.com>
2025-01-11 10:17:44 +0530

    UPDATE - dynamic dropdown population

--- Assets/Script/MainScript.cs
+++ Assets/Script/MainScript.cs
@@ -11,7 +11,12 @@ public class MainScript : MonoBehaviour
     [SerializeField] GameObject eventSystemOculus;
     [SerializeField] GameObject cameraUnity;
     [SerializeField] GameObject cameraOculus;
-
+    //[SerializeField] Dictionary<string, string> inputObjectNamePairs;
+    //= { "aircraft", "penguin", "axe", "tralismesh" };
+    //= { "E 45 Aircraft_obj", "PenguinBaseMesh", "Axe", "07_01_2025_16_29_33_1" };
+    [SerializeField] private List<string> valueInDropdown;
+    [SerializeField] private List<string> objNameOnServer;
+    [SerializeField] private TMPro.TMP_Dropdown tMP_Dropdown;
 
     public void Awake()
     {
@@ -20,7 +25,8 @@ public class MainScript : MonoBehaviour
     }
     public void Start()
     {
-        
+        tMP_Dropdown.AddOptions(valueInDropdown);
+
 #if UNITY_EDITOR
         eventSystemUnity.SetActive(true);
         eventSystemOculus.SetActive(false);
@@ -39,16 +45,20 @@ public class MainScript : MonoBehaviour
     }
     public void ProcessUserInput(string transcription)
     {
-        if (transcription.ToLower().Contains("aircraft"))
+        foreach (var i in valueInDropdown)
         {
-            Debug.Log("It is an aircraft");
-            streamOBJImporter.objectName = "E 45 Aircraft_obj";
-        }
-        else if (transcription.ToLower().Contains("penguin"))
-        {
-            Debug.Log("It is a penguin");
-            streamOBJImporter.objectName = "PenguinBaseMesh";
+            if(transcription == i)
+            {
+                Debug.Log($"The object name on the server should be \"{objNameOnServer[valueInDropdown.IndexOf(i)]}\"");
+                streamOBJImporter.objectName = objNameOnServer[valueInDropdown.IndexOf(i)];
+            }
         }
         streamOBJImporter.DownloadExistingObject();
+    }
+
+    public void ProcessUserPrompt(string prompt)
+    {
+        streamOBJImporter.objectName = prompt;
+        streamOBJImporter.GenerateAndDownloadObject();
     }
 }
