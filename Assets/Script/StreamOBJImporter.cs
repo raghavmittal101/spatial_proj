dea9ef913302c2c1886f90757bb4036acb98544d
Raghav Mittal <raghavmittal101@users.noreply.github.com>
2025-01-11 10:20:13 +0530

    Add - send prompt for fetching from text-2-3d server

--- Assets/Script/StreamOBJImporter.cs
+++ Assets/Script/StreamOBJImporter.cs
@@ -10,7 +10,8 @@ using UnityEngine.Networking;
 
 public class StreamOBJImporter : MonoBehaviour {
     public string objectName;
-    [SerializeField] private string hostURL = "http://100.64.0.1:5000/";
+    [SerializeField] private string predefinedObjHostURL = "http://100.64.0.1:5000/";
+    [SerializeField] private string text23DHostURL = null;
     [SerializeField] private string apiVersion = "v0";
     
     [SerializeField] private TextMeshProUGUI statusTextVariable;
@@ -28,9 +29,10 @@ public class StreamOBJImporter : MonoBehaviour {
     private string objURL;
     private string zipPath;
     private string downloadedMeshDirPath;
-    private MemoryStream obj_memStream;
-    private MemoryStream mtl_memStream;
-    private MemoryStream tex_memStream;
+    private string text23DGenURL;
+    //private MemoryStream obj_memStream;
+    //private MemoryStream mtl_memStream;
+    //private MemoryStream tex_memStream;
 
     [System.Serializable]
     public class ComponentURLs
@@ -40,13 +42,14 @@ public class StreamOBJImporter : MonoBehaviour {
         public string tex_file;
     }
 
-    private ComponentURLs componentURLs;
-    private Stream[] objComponentsMemStreams;
+    //private ComponentURLs componentURLs;
+    //private Stream[] objComponentsMemStreams;
 
 	void Start () { 
-        objFileFetchURL = hostURL + apiVersion + "/download-asset/"; // Replace with your API endpoint
-        objZipFetchURL =  hostURL + apiVersion + "/download-asset-compressed/";
-        objComponentsMemStreams = new Stream[3];
+        objFileFetchURL = predefinedObjHostURL + apiVersion + "/download-asset/"; // Replace with your API endpoint
+        objZipFetchURL =  predefinedObjHostURL + apiVersion + "/download-asset-compressed/";
+        //objComponentsMemStreams = new Stream[3];
+        text23DGenURL = text23DHostURL + apiVersion + "/text-2-3d/";
         //DownloadExistingObject();
 
     }
@@ -62,28 +65,29 @@ public class StreamOBJImporter : MonoBehaviour {
         errorTextObj.GetComponent<TextMeshProUGUI>().text = $"-> {message}";
     }
 
-    private IEnumerator RenderDownloadedMesh(string objName){
+    private IEnumerator RenderDownloadedMesh(string objName, InputMode inputMode){
         
-        downloadedMeshDirPath = Path.Combine(Application.persistentDataPath, objName);
-        string objfilePath = downloadedMeshDirPath + "/" + objName + ".obj";
-        string mtlfilePath = downloadedMeshDirPath + "/" + objName + ".mtl";
-        Debug.Log(objfilePath);
+        
         try
         {
             statusPanel?.SetActive(true);
-            yield return StartCoroutine(DownloadAndExtractZip(objName));
+            yield return StartCoroutine(DownloadAndExtractZip(objName, inputMode));
         }
         finally
         { }
         
         try
         {
+            downloadedMeshDirPath = Path.Combine(Application.persistentDataPath, zipURL.asset_name);
+            string objfilePath = downloadedMeshDirPath + "/" + zipURL.asset_name + ".obj";
+            string mtlfilePath = downloadedMeshDirPath + "/" + zipURL.asset_name + ".mtl";
+            Debug.Log(objfilePath);
             statusTextVariable.text = "Processing the object for rendering...";
             var loadedObj = new OBJLoader().Load(objfilePath, mtlfilePath);
             var grabbableObj = Instantiate(grabbableObjContainerPrefab, objPrefabPlaceholder.transform);
             grabbableObj.transform.position = new Vector3(
                 objPrefabPlaceholder.transform.position.x,
-                objPrefabPlaceholder.transform.position.y+0.03f,
+                objPrefabPlaceholder.transform.position.y+0.1f,
                 objPrefabPlaceholder.transform.position.z);
             Transform grabbableObjBoundingCubeTransform = grabbableObj.transform.GetChild(0).transform;
             Bounds boundsOfLoadedObj = _GetChildRendererBounds(loadedObj);
@@ -94,9 +98,10 @@ public class StreamOBJImporter : MonoBehaviour {
             loadedObj.transform.localScale = new Vector3(1, 1, 1);
             
         }
-        catch
+        catch(System.Exception e)
         {
             Debug.Log("There is some issue was rendering the object.");
+            Debug.LogError(e);
             PrintErrorToScreen("There was some issue in rendering the object.");
         }
         finally
@@ -141,14 +146,14 @@ public class StreamOBJImporter : MonoBehaviour {
 
     }
 
-    private IEnumerator RenderStreamedMesh(string objName){
-        yield return StartCoroutine(GetObjComponentsURLs(objName));
-        yield return StartCoroutine(DownloadFile(hostURL + componentURLs.mtl_file, 0)); // mtl_file
-        yield return StartCoroutine(DownloadFile(hostURL + componentURLs.tex_file, 1)); // tex_file
-        yield return StartCoroutine(DownloadFile(hostURL + componentURLs.obj_file, 2)); // obj_file
-        Debug.Log("now running ObjLoader......");
-        var loadedObj = new OBJLoader().Load(objComponentsMemStreams[2], objComponentsMemStreams[0]);
-    }
+    //private IEnumerator RenderStreamedMesh(string objName){
+    //    yield return StartCoroutine(GetObjComponentsURLs(objName));
+    //    yield return StartCoroutine(DownloadFile(hostURL + componentURLs.mtl_file, 0)); // mtl_file
+    //    yield return StartCoroutine(DownloadFile(hostURL + componentURLs.tex_file, 1)); // tex_file
+    //    yield return StartCoroutine(DownloadFile(hostURL + componentURLs.obj_file, 2)); // obj_file
+    //    Debug.Log("now running ObjLoader......");
+    //    var loadedObj = new OBJLoader().Load(objComponentsMemStreams[2], objComponentsMemStreams[0]);
+    //}
 
     private Bounds _GetChildRendererBounds(GameObject go)
     {
@@ -166,52 +171,74 @@ public class StreamOBJImporter : MonoBehaviour {
         else { return new Bounds(); }
     }
 
-    private IEnumerator GetObjComponentsURLs(string objName)
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("asset_name", objName);
-        using (var w = UnityWebRequest.Post(objFileFetchURL, form))
-        {
-            yield return w.SendWebRequest();
-            if (w.result != UnityWebRequest.Result.Success) {
-                Debug.Log(w.error);
-            }
-            else {
-                Debug.Log("finished");
-                componentURLs = JsonUtility.FromJson<ComponentURLs>(w.downloadHandler.text);
-            }
-        }
-    }
+    //private IEnumerator GetObjComponentsURLs(string objName)
+    //{
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("asset_name", objName);
+    //    using (var w = UnityWebRequest.Post(objFileFetchURL, form))
+    //    {
+    //        yield return w.SendWebRequest();
+    //        if (w.result != UnityWebRequest.Result.Success) {
+    //            Debug.Log(w.error);
+    //        }
+    //        else {
+    //            Debug.Log("finished");
+    //            componentURLs = JsonUtility.FromJson<ComponentURLs>(w.downloadHandler.text);
+    //        }
+    //    }
+    //}
 
-    private IEnumerator DownloadFile(string fileURL, int streamsArrayIndex){
-        Debug.Log("Downloading a file...");
-        MemoryStream textStream;
-        using(var www = new WWW(fileURL)){
-            yield return www;
-            textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.text));
-            Debug.Log("downloading for "+streamsArrayIndex+" done! ");
-            objComponentsMemStreams[streamsArrayIndex] = textStream;
-            Debug.Log(Encoding.UTF8.GetByteCount(www.text));
-        }
+    //private IEnumerator DownloadFile(string fileURL, int streamsArrayIndex){
+    //    Debug.Log("Downloading a file...");
+    //    MemoryStream textStream;
+    //    using(var www = new WWW(fileURL)){
+    //        yield return www;
+    //        textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.text));
+    //        Debug.Log("downloading for "+streamsArrayIndex+" done! ");
+    //        objComponentsMemStreams[streamsArrayIndex] = textStream;
+    //        Debug.Log(Encoding.UTF8.GetByteCount(www.text));
+    //    }
         
-    }
+    //}
 
 
     [System.Serializable]
     public class ZipURL
     {
         public string zip_file;
+        public string asset_name;
     }
 
     private ZipURL zipURL;
+    private enum InputMode
+    {
+        prompt,
+        objectName
+    }
 
     // Downloads zip file from server to persistent path and returns the folder path
-    private IEnumerator DownloadAndExtractZip(string objName){
+    private IEnumerator DownloadAndExtractZip(string objName, InputMode inputMode){
         statusTextVariable.text = "Fetching the object URL...";
         WWWForm form = new WWWForm();
-        form.AddField("asset_name", objName);
-        using (var w = UnityWebRequest.Post(objZipFetchURL, form))
+        string objectFetchURL = "";
+        string hostURL = "";
+
+        if(inputMode == InputMode.objectName)
         {
+            form.AddField("asset_name", objName);
+            objectFetchURL = objZipFetchURL;
+            hostURL = predefinedObjHostURL;
+        }
+        else if(inputMode == InputMode.prompt)
+        {
+            form.AddField("prompt", objName);
+            objectFetchURL = text23DGenURL;
+            hostURL = text23DHostURL;
+        }
+        
+        using (var w = UnityWebRequest.Post(objectFetchURL, form))
+        {
+            Debug.Log(objectFetchURL);
             yield return w.SendWebRequest();
             if (w.result != UnityWebRequest.Result.Success) {
                 Debug.Log(w.error);
@@ -224,7 +251,7 @@ public class StreamOBJImporter : MonoBehaviour {
                 zipURL = JsonUtility.FromJson<ZipURL>(w.downloadHandler.text);
 
                 // now download the file
-                zipPath = Path.Combine(Application.persistentDataPath, objName+".zip");
+                zipPath = Path.Combine(Application.persistentDataPath, zipURL.asset_name+".zip");
                 statusTextVariable.text = "Downloading the compressed object...";
                 using(UnityWebRequest www = UnityWebRequest.Get(hostURL + zipURL.zip_file))
                 {
@@ -299,37 +326,47 @@ public class StreamOBJImporter : MonoBehaviour {
     }
     private ConvertedFileURL convertedFileURL;
 
-    public IEnumerator UploadFileAndConvert(string filename, string filePath){
-        // List formData = new List();
-        byte[] filedata = File.ReadAllBytes(filePath);
-        // files[0] = files[0].Replace(@“.",”");
-        // formData.Add(new MultipartFormFileSection(“file”, bytes, files[0], “text/plain”));
-        // StartCoroutine(UploadFile(formData));
-        var convertTo3dUrl= hostURL + apiVersion + "/convert-to-3d/";
-        WWWForm form = new WWWForm();
-        form.AddBinaryData("file", filedata, filename);
-        using (var w = UnityWebRequest.Post(convertTo3dUrl, form))
-        {
-            yield return w.SendWebRequest();
-            if (w.result != UnityWebRequest.Result.Success) {
-                Debug.Log(w.error);
-            }
-            else {
-                Debug.Log("finished");
-                convertedFileURL = JsonUtility.FromJson<ConvertedFileURL>(w.downloadHandler.text);
-            }
-        }
+    //public IEnumerator UploadFileAndConvert(string filename, string filePath){
+    //    // List formData = new List();
+    //    byte[] filedata = File.ReadAllBytes(filePath);
+    //    // files[0] = files[0].Replace(@“.",”");
+    //    // formData.Add(new MultipartFormFileSection(“file”, bytes, files[0], “text/plain”));
+    //    // StartCoroutine(UploadFile(formData));
+    //    var convertTo3dUrl= hostURL + apiVersion + "/convert-to-3d/";
+    //    WWWForm form = new WWWForm();
+    //    form.AddBinaryData("file", filedata, filename);
+    //    using (var w = UnityWebRequest.Post(convertTo3dUrl, form))
+    //    {
+    //        yield return w.SendWebRequest();
+    //        if (w.result != UnityWebRequest.Result.Success) {
+    //            Debug.Log(w.error);
+    //        }
+    //        else {
+    //            Debug.Log("finished");
+    //            convertedFileURL = JsonUtility.FromJson<ConvertedFileURL>(w.downloadHandler.text);
+    //        }
+    //    }
 
-    }
+    //}
 
-    public void UploadFileAndConvert(){
-        StartCoroutine(UploadFileAndConvert("abc.png", "/home/raghav/Downloads/spatialsuiteSystem1.drawio (2).png"));
-    }
+    //public void UploadFileAndConvert(){
+    //    StartCoroutine(UploadFileAndConvert("abc.png", "/home/raghav/Downloads/spatialsuiteSystem1.drawio (2).png"));
+    //}
 
     public void DownloadExistingObject(){
-        StartCoroutine(RenderDownloadedMesh(objectName));
+        StartCoroutine(RenderDownloadedMesh(objectName, InputMode.objectName));
         var errorCount = errorScrollbarContainer.transform.childCount;
         for(int i=0; i<errorCount; i++)
+        {
+            Destroy(errorScrollbarContainer.transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void GenerateAndDownloadObject()
+    {
+        StartCoroutine(RenderDownloadedMesh(objectName, InputMode.prompt));
+        var errorCount = errorScrollbarContainer.transform.childCount;
+        for (int i = 0; i < errorCount; i++)
         {
             Destroy(errorScrollbarContainer.transform.GetChild(i).gameObject);
         }
